12f5029c08cbb1f6cb5c76f0bca51b525b828818
franciscoryb1 <fraciscoryb1@hotmail.com>
2024-05-20 21:58:47 -0300

    ADD Modal y funcionalidad al boton Agregar Usuario

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
@@ -13,40 +13,40 @@ let tablaData;
 $(function () {
     tablaData = $('#tbdata').DataTable({
         responsive: true,
-         "ajax": {
-             "url": '/Usuario/Lista',
-             "type": "GET",
-             "datatype": "json"
-         },
-         "columns": [
-             { "data": "idUsuario", "visible":false, "searchable":false },
-             //{
-             //    "data": "urlFoto", render: function (data) {
-             //        return `<img style="height:60px" src=${data} class="rounded mx-auto d-block"/>`
-             //    }
-             //},
-             { "data": "urlFoto"},
-             { "data": "nombre" },
-             { "data": "correo" },
-             { "data": "telefono" },
-             { "data": "nombreRol" },
-             {
-                 "data": "esActivo", render: function (data) {
-                     if (data == 1)
-                         return '<span class="badge badge-info">Activo<span/>';
-                     else
-                         return '<span class="badge badge-danger">No Activo<span/>';
-                 }
-             },
-             {
-                 "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
-                     '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
-                 "orderable": false,
-                 "searchable": false,
-                 "width": "80px"
-             }
-         ],
-         order: [[0, "desc"]],
+        "ajax": {
+            "url": '/Usuario/Lista',
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "idUsuario", "visible": false, "searchable": false },
+            //{
+            //    "data": "urlFoto", render: function (data) {
+            //        return `<img style="height:60px" src=${data} class="rounded mx-auto d-block"/>`
+            //    }
+            //},
+            { "data": "urlFoto" },
+            { "data": "nombre" },
+            { "data": "correo" },
+            { "data": "telefono" },
+            { "data": "nombreRol" },
+            {
+                "data": "esActivo", render: function (data) {
+                    if (data == 1)
+                        return '<span class="badge badge-info">Activo<span/>';
+                    else
+                        return '<span class="badge badge-danger">No Activo<span/>';
+                }
+            },
+            {
+                "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
+                    '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
+                "orderable": false,
+                "searchable": false,
+                "width": "80px"
+            }
+        ],
+        order: [[0, "desc"]],
         dom: "Bfrtip",
         buttons: [
             {
@@ -63,4 +63,22 @@ $(function () {
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
         },
     });
+});
+
+function mostratModal(modelo = MODELO_BASE) {
+    $("#txtId").val(modelo.idUsuario)
+    $("#txtNombre").val(modelo.nombre)
+    $("#txtCorreo").val(modelo.correo)
+    $("#txtTelefono").val()
+    $("#txtTelefono").val(modelo.telefono)
+    $("#cboRol").val(modelo.idRol == 0 ? $("#cboRol option:first").val() : modelo.idRol)
+    $("#cboEstado").val(modelo.esActivo)
+    $("#txtFoto").val("")
+    $("#imgUsuario").attr("src", modelo.urlFoto)
+
+    $("#modalData").modal("show")
+}
+
+$("#btnNuevo").on("click", function () {
+    mostratModal()
 })
