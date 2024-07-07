00ec580f42e18b5bd9ef2b46bc6bb56f1ebc224e
franciscoryb1 <fraciscoryb1@hotmail.com>
2024-07-06 21:01:53 -0300

    Servicio y controlador Categoria. Javascript Categoria Index. Segun 14

---
+++ SistemaVenta.BLL/Interfaces/ICategoriaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.BLL.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<List<Categoria>> Lista();
+        Task<Categoria>Crear(Categoria categoria);
+        Task<Categoria>Editar(Categoria categoria);
+        Task<bool>Eliminar(int idCategoria);
+    }
+}
