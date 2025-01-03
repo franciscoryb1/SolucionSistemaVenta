73e1083275c2270d13bc1b9a846e49a2b02b98b8
franciscoryb1 <133237481+franciscoryb1@users.noreply.github.com>
2025-01-02 22:12:11 -0300

    ADD Cambiar informacion de usuario y contraseña en HomeController. Vistaa Perfil, Home_Perfil.js

--- SistemaVenta.AplicacionWeb/Controllers/HomeController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/HomeController.cs
@@ -5,6 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 using SistemaVenta.AplicacionWeb.Models;
 using System.Diagnostics;
 
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+
 using System.Security.Claims;
 
 namespace SistemaVenta.AplicacionWeb.Controllers
@@ -12,12 +18,15 @@ namespace SistemaVenta.AplicacionWeb.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUsuarioService _usuarioServicio;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUsuarioService usuarioServicio, IMapper mapper)
         {
-            _logger = logger;
+            _usuarioServicio = usuarioServicio;
+            _mapper = mapper;
         }
+
 
         public IActionResult Index()
         {
@@ -33,6 +42,102 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerUsuario()
+        {
+            GenericResponse<VMUsuario> response = new GenericResponse<VMUsuario>();
+
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c=>c.Type==ClaimTypes.NameIdentifier)
+                    .Select(c=>c.Value).SingleOrDefault();
+
+                VMUsuario usuario = _mapper.Map<VMUsuario>(await _usuarioServicio.ObtenerPorId(int.Parse(idUsuario)));
+
+                response.Estado = true;
+                response.Objeto = usuario;
+            }
+            catch (Exception ex) {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, response);
+            
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> GuardarPerfil([FromBody] VMUsuario modelo)
+        {
+            GenericResponse<VMUsuario> response = new GenericResponse<VMUsuario>();
+
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.NameIdentifier)
+                    .Select(c => c.Value).SingleOrDefault();
+
+                Usuario entidad = _mapper.Map<Usuario>(modelo);
+
+                entidad.IdUsuario = int.Parse(idUsuario);
+
+                bool resultado = await _usuarioServicio.GuardarPerfil(entidad);
+
+                response.Estado = resultado;
+            }
+            catch (Exception ex)
+            {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, response);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CambiarClave([FromBody] VMCambiarClave modelo)
+        {
+            GenericResponse<bool> response = new GenericResponse<bool>();
+
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.NameIdentifier)
+                    .Select(c => c.Value).SingleOrDefault();
+
+
+                bool resultado = await _usuarioServicio.CambiarClave(
+                    int.Parse(idUsuario),
+                    modelo.claveActual,
+                    modelo.claveNueva
+                );
+
+                response.Estado = resultado;
+            }
+            catch (Exception ex)
+            {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, response);
+
+        }
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
